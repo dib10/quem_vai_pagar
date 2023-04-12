ba1550665e8edec813d928fc01886d4df99c7d77
Caio <129826533+dib10@users.noreply.github.com>
2023-04-12 13:05:08 -0300

    Delete calculadora IMC.py

--- calculadora IMC.py
+++
@@ -1,6 +0,0 @@
-print('Bem vindo a calculadora de IMC')
-peso = float(input('Digite seu peso (Kg): '))
-altura = float(input('Digite sua altura (M): '))
-imc = peso / (altura **2)
-imc_truncado = imc.__trunc__()
-print(f'O imc do índividuo equivale a {imc_truncado}')

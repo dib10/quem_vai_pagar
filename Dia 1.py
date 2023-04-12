4e24c8ef28f1aed1dee76101613446e8976249e6
Caio <129826533+dib10@users.noreply.github.com>
2023-04-12 13:04:06 -0300

    Delete Dia 1.py

--- Dia 1.py
+++
@@ -1,4 +0,0 @@
-print('Bem vindo(a) ao gerador de nome de banda!')
-cidade = str(input('Qual é o nome da cidade em que voce nasceu?'))
-pet = str(input('Qual é o nome do seu pet?'))
-print(f'O nome da sua banda pode ser {cidade} {pet}')

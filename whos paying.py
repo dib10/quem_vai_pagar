b6e22dfc667fe460ced91ee47abdaf97f95600d2
Caio <129826533+dib10@users.noreply.github.com>
2023-04-12 13:05:51 -0300

    Update whos paying.py

--- whos paying.py
+++ whos paying.py
@@ -1,14 +1,19 @@
 import random
-
+import time
 lista = []
+print('Quem vai pagar a conta?💸')
 while True:
-    nome= str(input('Digite um nome: '))
-    lista.append(nome)
-    resposta = str(input('Quer continuar?[S/N]: ')).upper().strip()[0]
-    if resposta not in 'SN':
-        print('Digite apenas S ou N como resposta')
-    elif resposta == '':
-            print('Digite uma resposta válida')
-    elif resposta == 'N':
-        break
-print(f'O sorteado dessa lista foi {random.choice(lista)}')
+    nome = input('👤Digite um nome: ').strip()
+    if nome:  # verifica se o nome não está vazio
+        lista.append(nome)
+        resposta = input('Quer continuar? [S/N]: ').strip().upper()
+        if resposta == 'N':
+            break
+        elif resposta != 'S':
+            print('❌Digite apenas S ou N como resposta')
+
+print('\n⌛Estamos sorteando quem pagará a conta...\n')
+time.sleep(3)
+sorteado = random.choice(lista)
+print(f'😎💰O sorteado dessa lista foi: {sorteado.title()}\n')
+print(f'{sorteado.title()}, todos lembrarão dessa boa ação! 😉')

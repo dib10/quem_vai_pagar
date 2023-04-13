c627d373ab3f45fc591b0904b67924498b9e1af3
Caio <129826533+dib10@users.noreply.github.com>
2023-04-13 13:25:07 -0300

    Update quempaga.py

--- quempaga.py
+++ quempaga.py
@@ -1,19 +1,33 @@
 import random
 import time
+
 lista = []
+
 print('Quem vai pagar a conta?💸')
+
 while True:
     nome = input('👤Digite um nome: ').strip()
+
     if nome:  # verifica se o nome não está vazio
         lista.append(nome)
-        resposta = input('Quer continuar? [S/N]: ').strip().upper()
+        resposta = ''
+
+        while resposta not in ['S', 'N']:  # aguarda uma resposta válida
+            resposta = input('Quer continuar? [S/N]: ').strip().upper()
+
+            if resposta not in ['S', 'N']:
+                print('❌Digite apenas S ou N como resposta')
+
         if resposta == 'N':
             break
-        elif resposta != 'S':
-            print('❌Digite apenas S ou N como resposta')
 
 print('\n⌛Estamos sorteando quem pagará a conta...\n')
 time.sleep(3)
-sorteado = random.choice(lista)
-print(f'😎💰O sorteado dessa lista foi: {sorteado.title()}\n')
-print(f'{sorteado.title()}, todos lembrarão dessa boa ação! 😉')
+
+if lista:
+    sorteado = random.choice(lista)
+    print(f'😎💰O sorteado dessa lista foi: {sorteado.title()}\n')
+    print(f'{sorteado.title()}, todos lembrarão dessa boa ação! 😉')
+else:
+    print('❌Não há nomes na lista para sorteio. Tente novamente.')
+
